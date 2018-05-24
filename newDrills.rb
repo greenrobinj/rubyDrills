f1b1a488b0e97c01d8b821679a53d2a5a6544e7a
Robin Green <greenrj4@gmail.com>
2018-05-24 09:20:16 -0600

    init

---
+++ newDrills.rb
@@ -0,0 +1,40 @@
+# require pry
+
+# alphabetical order
+# =========================
+# def alpha
+#   s = gets.strip.chars.sort_by(&:downcase).join
+# puts s
+# end
+# alpha
+
+
+#palindrome
+#=======================
+# def pal
+#   x = gets.strip
+#   y = x.reverse()
+#   if x == y
+#     puts "NICE!! "
+#   else
+#     x != y
+#     puts "Not Today Zerg!!"
+#   end
+# end
+# pal 
+
+
+# prime  
+#=================
+# def prime
+#   x = gets.to_i
+#   puts x
+#   if x % x == 0
+#     puts "Prime"
+#   else
+#     puts "Not today Zerg!!"
+#   end
+
+# end
+# prime
+ 
